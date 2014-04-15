bcf568d002c7377dffa809c98db38cf4d821a9d3
Jason Lau <i@dotkrnl.com>
2014-04-15 11:13:08 +0800

    fix ui errors on legacy browsers

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -63,6 +63,7 @@ dotchat = {
    * Setup UI event controller
    */
   bind: function () {
+    // TODO: all binding functions here should be in handlers
     window.onscroll = dotchat.handlers.scrollChanged;
     var enter_key = 13; // disable enter and used as send
     $$('chat_content').onkeydown =
@@ -113,13 +114,11 @@ dotchat = {
         content: $$('chat_content').value
       }, function (info) {
         if (info.ok) {
-          $$('chat_content').disabled = false;
           $$('chat_content').value = '';
         }
       });
       // TODO: show a pending message
       // dotchat.newMessage($$('chat_content').value, true);
-      $$('chat_content').disabled = true;
       $$('chat_content').value = 'Sending: ' + $$('chat_content').value;
     },
 
@@ -282,7 +281,11 @@ dotchat = {
      * @returns {string}
      */
     valueOf: function(dom, key) {
-      return dom.getElementsByClassName(key)[0].innerHTML;
+      try {
+        return dom.getElementsByClassName(key)[0].innerHTML;
+      } catch (e) {
+        return IEGetElementsByClassName(dom, key)[0].innerHTML;
+      }
     },
 
     /**
@@ -387,3 +390,13 @@ dotchat = {
  |*|  * docCookies.hasItem(name)
  \*/
 var docCookies = { getItem: function (sKey) { return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null; }, setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) { if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; } var sExpires = ""; if (vEnd) { switch (vEnd.constructor) { case Number: sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd; break; case String: sExpires = "; expires=" + vEnd; break; case Date: sExpires = "; expires=" + vEnd.toUTCString(); break; } } document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : ""); return true; }, removeItem: function (sKey, sPath, sDomain) { if (!sKey || !this.hasItem(sKey)) { return false; } document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + ( sDomain ? "; domain=" + sDomain : "") + ( sPath ? "; path=" + sPath : ""); return true; }, hasItem: function (sKey) { return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie); } };
+
+var IEGetElementsByClassName = function (node, classname) {
+  var a = [];
+  var re = new RegExp('(^| )'+classname+'( |$)');
+  var els = node.getElementsByTagName("*");
+  for (var i = 0, j = els.length; i < j; i++) {
+    if (re.test(els[i].className)) a.push(els[i]);
+  }
+  return a;
+};
